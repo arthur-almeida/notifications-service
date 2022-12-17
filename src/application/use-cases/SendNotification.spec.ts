4a5cd24217d701d36449c79fff8c41e925ce4e33
Arthur Almeida <arthurdosantosalmeida@gmail.com>
2022-12-17 14:56:39 -0300

    refactor: move notification repository in memory to its own file

--- src/application/use-cases/SendNotification.spec.ts
+++ src/application/use-cases/SendNotification.spec.ts
@@ -1,24 +1,18 @@
-import { Notification } from '../entities/Notification';
 import { SendNotification } from './SendNotification';
-
-const notifications: Notification[] = [];
-
-const notificationRepository = {
-  async create(notification: Notification) {
-    notifications.push(notification);
-  },
-};
+import { InMemoryNotificationRepository } from '../../../test/repositories/in-memory-notification.repository';
 
 describe('SendNotification', () => {
   it('should be able to send a notification', async () => {
+    const notificationRepository = new InMemoryNotificationRepository();
     const sendNotification = new SendNotification(notificationRepository);
 
-    await sendNotification.execute({
+    const { notification } = await sendNotification.execute({
       recipientId: 'valid-recipient-id',
       category: 'social',
       content: 'this is a notification',
     });
 
-    expect(notifications).toHaveLength(1);
+    expect(notificationRepository.notifications).toHaveLength(1);
+    expect(notificationRepository.notifications[0]).toEqual(notification);
   });
 });
